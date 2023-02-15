564a12ee3d793d647401b350b3a664c6f7e2e2d8
JMRBDV <jrodbon664@g.educaand.es>
2023-02-15 18:45:48 +0100

    Movimiento Aereo
    Movimiento aereo añadido, salto completado

--- Assets/Scripts/Player/PlayerMovement.cs
+++ Assets/Scripts/Player/PlayerMovement.cs
@@ -1,16 +1,18 @@
+using System;
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.Serialization;
 
 [RequireComponent(typeof(Rigidbody2D), typeof(BoxCollider2D))]
 public class PlayerMovement : MonoBehaviour
 {
     [SerializeField]
-    private float movementSpeed;
+    private float movementAcceleration = 2, maxSpeed = 5;
+    
     
     private State _state = State.IDLE;
     private MovementState _movementState = MovementState.STAND;
-    private bool _jump = false;
     private Vector2 _directionLooking = new Vector2(0, 0);
 
     private Rigidbody2D _rigidbody2D;
@@ -19,6 +21,7 @@ public class PlayerMovement : MonoBehaviour
     void Start()
     {
         _rigidbody2D = GetComponent<Rigidbody2D>();
+        _rigidbody2D.isKinematic = false;
         
         _state = State.IDLE;
         _movementState = MovementState.STAND;
@@ -27,7 +30,9 @@ public class PlayerMovement : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        Vector3 direction = Vector3.zero;
+        Vector2 directionMovement = Vector3.zero;
+        Debug.Log(_state);
+        Debug.Log(_movementState);
 
         switch (_state)
         {
@@ -36,6 +41,7 @@ public class PlayerMovement : MonoBehaviour
                 break;
             
             case State.MOVING:
+                Debug.Log(_movementState);
                 
                 // Update direction if can move
                 if (Input.GetKey(KeyCode.W))
@@ -45,7 +51,7 @@ public class PlayerMovement : MonoBehaviour
                         
                 if (Input.GetKey(KeyCode.A))
                 {
-                    direction += Vector3.left;
+                    directionMovement += Vector2.left;
                     _directionLooking = Vector2.left;
                 }
                         
@@ -56,46 +62,66 @@ public class PlayerMovement : MonoBehaviour
                 
                 if (Input.GetKey(KeyCode.D))
                 {
-                    direction += Vector3.right;
-                    _directionLooking = Vector2.left;
+                    directionMovement += Vector2.right;
+                    _directionLooking = Vector2.right;
                 }
-                direction = direction.normalized;
+                directionMovement = directionMovement.normalized;
+
+                if (_rigidbody2D.velocity.x < -maxSpeed)
+                {
+                    _rigidbody2D.AddForce(directionMovement.x > 0 ? directionMovement * movementAcceleration : Vector2.zero);
+                }
+                else if (_rigidbody2D.velocity.x > maxSpeed)
+                {
+                    _rigidbody2D.AddForce(directionMovement.x < 0 ? directionMovement * movementAcceleration : Vector2.zero);
+                }
+                else
+                {
+                    _rigidbody2D.AddForce(directionMovement * movementAcceleration);
+                }
 
                 if (Input.GetKey(KeyCode.Space))
                 {
-                    _jump = true;
+                    if (_movementState is MovementState.WALK or MovementState.STAND)
+                    {
+                        _movementState = MovementState.JUMP;
+                    }
                 }
                 
                 switch (_movementState)
                 {
                     case MovementState.STAND:
-                        if (direction != Vector3.zero)
-                        {
-                            _movementState = MovementState.WALK;
-                        }
+                        // Debug.Log("Movement state: standing");
+                        // Debug.Log(directionMovement);
+                        // Debug.Log(_directionLooking);
+                        if (_rigidbody2D.velocity.y < 0)
+                            _movementState = MovementState.FALL;
+
+
                         // if touches floor movsta = JUMP
                         
                         break;
-                    
-                    case MovementState.WALK:
-                        if (direction == Vector3.zero)
+
+                    case MovementState.FALL:
+                        if (_rigidbody2D.velocity.y == 0)
                         {
                             _movementState = MovementState.STAND;
                         }
-
-                        transform.position += direction * (movementSpeed * Time.deltaTime);
-                        break;
-                    
-                    case MovementState.FALL:
-                        // on collision with floor state = stand.
-                        _movementState = MovementState.STAND;
+                        
                         break;
                     
                     case MovementState.JUMP:
-                        _rigidbody2D.AddForce(new Vector2(0, 2 * movementSpeed));
+                        Debug.Log("Hello");
+                        if (_rigidbody2D.velocity.y == 0)
+                            _rigidbody2D.velocity += new Vector2(0, 3*movementAcceleration);
+                            // _rigidbody2D.AddForce(new Vector2(0, 50 * movementAcceleration)); 
+                        
+                        
                         _movementState = MovementState.FALL;
                         break;
-                    
+
+                    default:
+                        throw new ArgumentOutOfRangeException();
                 }
                 
                 break;
@@ -103,6 +129,14 @@ public class PlayerMovement : MonoBehaviour
             case State.ATTACKING:
                 break;
         }
+        Debug.Log(_state);
+        Debug.Log(_movementState);
+    }
+
+    
+    void LookAt(Vector2 lookDirection)
+    {
+
     }
 
     void UpdateActions()

3e4434d7a777ec066183a1301981bf62abf934f0
Jrommon <jrommon566@g.educaand.es>
2023-02-21 16:35:14 +0100

    Mejora de float
    He reemplazado el script "float" que tenía el objeto DoubleJump por una animación que soluciona los fallos que tenía el script.

--- Assets/Scripts/PowerUp/PowDoubleJump.cs
+++ Assets/Scripts/PowerUp/PowDoubleJump.cs
@@ -2,15 +2,15 @@ using UnityEngine;
 
 public class PowDoubleJump : MonoBehaviour
 {
-    [SerializeField] private string PlayerTag = "Player";
+    private const string PlayerTag = "Player";
     private void OnTriggerEnter2D(Collider2D col)
     {
-        Debug.Log($"Tag del jugador: {PlayerTag} \n Tag del colisionador: {col.tag}");
+        //Habilitar en esta seccion el flag de salto doble.
+        
+        //Debug.Log($"Tag del jugador: {PlayerTag} \n Tag del colisionador: {col.tag}");
         if (col.CompareTag(PlayerTag))
         {
             Destroy(this.gameObject);
         }
-        
-        //Habilitar en esta seccion el flag de salto doble.
     }
 }

c9131618c602ad27439089c8a076e3c75a5b81d5
AdrianBejarano <57682133+AdrianBejarano@users.noreply.github.com>
2023-02-22 18:25:58 +0100

    Cambios Para mejorar Funcionalidad

--- Assets/Scripts/Enemies/Kappa_Snappa.cs
+++ Assets/Scripts/Enemies/Kappa_Snappa.cs
@@ -1,4 +1,4 @@
-using System;
+//using System;
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
@@ -17,11 +17,14 @@ public class Kappa_Snappa : MonoBehaviour
     [SerializeField] [Range(-1,1)] private int _direction = 1;
     //Velocidad de movimiento
     [SerializeField] private int _speed;
-    
 
-    
+    private RaycastHit2D _ray;
 
-    
+
+
+
+
+
     void Update()
     {
         //Mueve a la criatura hacia delante mientras no supere la velocidad
@@ -30,7 +33,7 @@ public class Kappa_Snappa : MonoBehaviour
             moveFoward();
         }
         //Gira a la criatura cuando  choca con una pared
-        
+        _ray = Physics2D.Raycast(transform.position, Vector2.right * _direction, 1, 3);
         turn();
         //Comprueba si el sprite debe estar girado
         _spriteRenderer.flipX = _direction == 1 ? true : false;
@@ -40,12 +43,14 @@ public class Kappa_Snappa : MonoBehaviour
 
     private void turn()
     {
-
-        if (Physics2D.Raycast(transform.position, Vector2.right * _direction, 1))
+        if (_ray)
         {
-            
+            if (_ray.collider.CompareTag("Floor"))
+            {
                 _direction = -_direction;
+            }
         }
+        
     }
 
     private void moveFoward()
